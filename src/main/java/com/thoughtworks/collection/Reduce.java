ccbdf9dc808438817d8e1b435345bced3fa4c8ab
Theo Lo <theo.lo@oocl.com>
2020-11-25 13:32:05 +0800

    [Stream][Theo] feat: Finish 3 tasks

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -3,6 +3,7 @@ package com.thoughtworks.collection;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reduce {
 
@@ -13,14 +14,21 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                        .mapToDouble(i -> i)
+                        .reduce(Double::sum)
+                        .getAsDouble()/arrayList.size();
     }
 
     public int getMaxValue() {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .reduce(0, Integer::max);
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .filter(item -> item%2 != 0)
+                .reduce((first,second)->second)
+                .orElse(null);
     }
 }
